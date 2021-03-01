4f6ab0a356019ebc7b2039f17bc31dc9a4a11f95
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-01 21:33:27 +0530

    Added Backgrounds

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
-include "raylib.h"
+#include "raylib.h"
 
 int main(void)
 {
-	int screenWidth = 640;
-	int screenHeight = 480;
+	const int screenWidth = 800;
+	const int screenHeight = 600;
+    
+    InitWindow(screenWidth, screenHeight, "Of Warriors And Monsters");
+    
+    Texture2D background = LoadTexture("assets/images/backgrounds/4.png");
+    Vector2 backgroundPosition = {0, 0};
+    
+    SetTargetFPS(60);
+    
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTextureEx (background, backgroundPosition, 0, 1.25, RAYWHITE);
+        
+        EndDrawing();
+    }
 }
