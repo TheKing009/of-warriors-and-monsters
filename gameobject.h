8557e3dd399ed7e3d563ca724c1a30052682610d
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-02 17:27:00 +0530

    Started Working on Scenes and GameObjects

---
+++ gameobject.h
@@ -0,0 +1,10 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+#endif
+
+class Gameobject
+{
+    private:
+    
+    public:
+}
---
+++ scene.h
@@ -0,0 +1,10 @@
+#ifndef SCENE_H
+#define SCENE_H
+#endif
+
+class Scene
+{
+    private :
+    
+    public:
+}
