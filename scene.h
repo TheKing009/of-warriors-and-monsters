764c0e39d743941041a6ad79b5e056b08ad3ab2c
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-03 15:49:24 +0530

    Added hash id to scene.h

--- scene.h
+++ scene.h
@@ -2,9 +2,22 @@
 #define SCENE_H
 #endif
 
+#include<functional>
+#include<string>
+
+#include "gameobject.h"
+
+using std::string;
+using std::hash;
+
 class Scene
 {
     private :
+    hash<string> _name;
     
     public:
-}
+    // Every scene needs a name to be initialized
+    Scene(string name) {_name(name);}
+    // if the name isn't initialized, then it is named as default.
+    Scene() {_name("default");}
+};
