24608b0a6198b914677395aa04abc776f0d58135
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-05 18:05:47 +0530

    Added a Dummy Component

---
+++ component.h
@@ -0,0 +1,14 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+#endif
+
+class Component
+{
+    private :
+    
+    public :
+    void Init();
+    void Update();
+    void Final();
+    void OnCreate();
+};
